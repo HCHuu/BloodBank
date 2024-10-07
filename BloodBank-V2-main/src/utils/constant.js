d25fc7c9a6fc8c62d33bcb3e42798b8213c559f9
ndhuy23 <dinhhuy230@gmail.com>
2024-10-07 12:08:43 +0700

    config: first deploy

--- BloodBank-V2-main/src/utils/constant.js
+++ BloodBank-V2-main/src/utils/constant.js
@@ -1,5 +1,5 @@
 export const PAGE_SIZE = 10;
-export const BASE_URL = "http://localhost:7067";
+export const BASE_URL = "http://192.168.20.112:7067";
 export const ROLE = {
   user: 0,
   hospital: 1,
