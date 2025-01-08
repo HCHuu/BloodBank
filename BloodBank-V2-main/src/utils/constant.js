2da0cab5ec0d45ae1d3a1b56bd9f5f8c769cfc5e
ndhuy23 <dinhhuy230@gmail.com>
2025-01-08 22:51:50 +0700

    upd: update port backend

--- BloodBank-V2-main/src/utils/constant.js
+++ BloodBank-V2-main/src/utils/constant.js
@@ -1,5 +1,5 @@
 export const PAGE_SIZE = 10;
-export const BASE_URL = "http://209.126.12.61:30597";
+export const BASE_URL = "http://209.126.12.61:30995";
 export const ROLE = {
   user: 0,
   hospital: 1,
