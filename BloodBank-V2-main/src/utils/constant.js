bb6198eb5e11a3cdad962a605871f3f98eec9563
ndhuy23 <radu_diaconeasa@yahoo.com>
2025-05-03 16:37:11 +0700

    upd: base url

--- BloodBank-V2-main/src/utils/constant.js
+++ BloodBank-V2-main/src/utils/constant.js
@@ -1,5 +1,5 @@
 export const PAGE_SIZE = 10;
-export const BASE_URL = "http://209.126.12.61:30995";
+export const BASE_URL = import.meta.env.VITE_CORE_BLOOD_BANK;
 export const ROLE = {
   user: 0,
   hospital: 1,
